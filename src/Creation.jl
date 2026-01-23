210f39d69f20bdd289e79797a94b161332796bfc
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-01-23 17:34:14 -0500

    + field non_cat_reason

--- src/Creation.jl
+++ src/Creation.jl
@@ -85,6 +85,7 @@ function fusion_ring(
     references                          = missing,
     software                            = missing,
     comments                            = missing,
+    non_cat_reason                      = missing,
     skip_check                          = false,
     )
 
@@ -123,6 +124,7 @@ function fusion_ring(
         ,references
         ,software
         ,comments
+        ,non_cat_reason
     )
 end
 
