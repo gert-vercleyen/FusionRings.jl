658ef6c20f937d44586974a363a37b353099950f
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-01-23 17:38:09 -0500

    moved code

--- src/GeneralFunctions.jl
+++ src/GeneralFunctions.jl
@@ -1,37 +1,3 @@
-# ┌────────────────────────────────────────────────────────────────────────────┐
-# │                              Pretty printing                               │
-# └────────────────────────────────────────────────────────────────────────────┘
-
-
-
-function transform_integer( i::Int, dict::Dict ) 
-  dgts = digits( i, base = 10 )
-  join( reverse( [ dict[j] for j in dgts  ] ) )
-end
-
-bold_integer(i::Int)::String = transform_integer(i, bold_digits_dict)
-
-bold_digits_dict = 
-  Dict(
-    0 => "𝟎", 1 => "𝟏", 2 => "𝟐", 3 => "𝟑", 4 => "𝟒", 
-    5 => "𝟓", 6 => "𝟔", 7 => "𝟕", 8 => "𝟖", 9 => "𝟗"
-  )
-
-subscript_integer(i::Int)::String = transform_integer(i,subs_digits_dict)
-
-subs_digits_dict = 
-  Dict(
-    0 => "₀", 1 => "₁", 2 => "₂", 3 => "₃", 4 => "₄", 
-    5 => "₅", 6 => "₆", 7 => "₇", 8 => "₈", 9 => "₉"
-  )
-
-superscript_integer(i::Int) = transform_integer(i,sup_digits_dict)
-
-sup_digits_dict = 
-  Dict(
-    0 => "⁰", 1 => "¹", 2 => "²", 3 => "³", 4 => "⁴", 
-    5 => "⁵", 6 => "⁶", 7 => "⁷", 8 => "⁸", 9 => "⁹"
-  )
 
 function is_constant_array( arr; equalfunc = === ) 
   if isempty(arr)
