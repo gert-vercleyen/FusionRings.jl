cd0eb92f948cf8148ed3bf3edec819558ec6b4d8
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-01-23 17:37:42 -0500

    + tex_reps

--- src/FormattingAndPrinting.jl
+++ src/FormattingAndPrinting.jl
@@ -93,3 +93,171 @@ function product_string(fr::FusionRing, a::Int, b::Int)
     end
     string(fr.element_names[a], " × ", fr.element_names[b], " = ", rhs)
 end
+
+function tex_reps( x::QQBarFieldElem; try_cyclo = false )
+    rat = rational_tex_rep(x)
+    if rat != ""
+        return Dict(
+            "rational"  => rat,
+            "radical"   => rat,
+            "power_sum" => rat,
+            "cyclo"     => rat,
+            "general"   => general_tex_rep(x)
+        )
+    end
+        
+    ps = power_sum_tex_rep(x)
+    if ps != ""
+        cyc = ps
+    elseif (ps == "") && try_cyclo
+        cyc = cyclo_tex_rep(x)
+    else
+        cyc = ""
+    end
+    
+    Dict(
+        "rational" => rational_tex_rep(x),
+        "radical" => radicals_tex_rep(x),
+        "power_sum" => ps,
+        "cyclo" => cyc,
+        "general" => general_tex_rep(x)
+    )
+end
+
+function general_tex_rep( x::QQBarFieldElem )
+    n = string( rootnum( x ) )
+
+    fix_poly_string("[" * string(minpoly(x)) * "]_{" * n * "}")
+end
+
+function fix_fractions(str::String)::String
+    replace( str, r"(?<p>\d+)//(?<q>\d+)" => s"\\frac{\g<p>}{\g<q>}" )
+end
+
+function fix_mult(str::String)::String
+    replace( str, "*" => "" )
+end
+
+function fix_spaces(str::String)::String
+    replace( str, " " => "" )
+end
+
+function fix_powers(str::String)::String
+    replace( str, r"\^(?<p>\d{2,})" => s"^{\g<p>}" )
+end
+
+function fix_cyclo(str::String)::String
+    replace( str, r"zeta\((?<deg>\d+)\)" => s"\\zeta_{\g<deg>}" )
+end
+
+function fix_poly_string(str::String)::String
+    ( fix_fractions ∘ fix_mult ∘ fix_spaces ∘ fix_powers )(
+        str
+    )
+end
+
+function cyclo_tex_rep(x::QQBarFieldElem)
+    cx, = to_composite_field( x )
+    if !is_abelian( parent( cx ) )
+        return ""
+    else
+        el = to_cyclotomic_field( cx ... )[1][1]
+        aQQb, = abelian_closure(QQ)
+
+        (fix_cyclo ∘ fix_poly_string ∘ string ∘ aQQb)(el)
+    end   
+end
+
+function radicals_tex_rep( x::QQBarFieldElem )
+    mp = minpoly( x )
+    d  = degree( mp )
+    if d == 1
+        mp, q = collect( coefficients( mp ) )
+        fix_poly_string( string( -mp//q ) )
+    elseif d == 2
+        # output string consists of 4 parts:
+        # <-b/2a> * <sign> * <factor/a2> * <sqrt{Δ/factor^2}>
+        #   s1    *   s2   *     s3      *        s4
+        QQb      = algebraic_closure( QQ )
+        c, b, a  = collect( coefficients( mp ) )
+        s1       = b == 0 ? "" : string(-b//(2*a))
+        
+        Δ        = b^2 - 4*a*c
+        pval     = (-QQb(b) + sqrt(QQb(Δ)))//(2*a)
+        s2       = pval == x ? "+" : "-" 
+
+        ( d, e ) = factor_squares(Δ)
+        s3       = d//(2*a) == 1 ? "" : string(d//(2*a))
+
+        s4       = string( "\\sqrt{", ( Δ > 0 ? e : -e ), "}" )
+
+        return fix_poly_string( string( s1, s2, s3, s4 ) )
+    else
+        ""
+    end
+end
+
+# writes an integer as a multiplication of a squared number
+# and a non-square
+function factor_squares(x)
+    zzx = ZZ(x)
+    sq_factors =
+        filter(
+            λ -> λ[2] > 1,
+            (collect ∘ factor)( zzx )
+        )
+    length( sq_factors ) == 0 && return (1,x)
+    
+    sq  = prod( a^ZZ( floor( ZZ(b)//2 ) ) for (a,b) in sq_factors )
+    nsq = zzx/sq^2
+
+    ( sq, nsq )
+end
+
+#TODO: surely there must be a better way...
+function rational_tex_rep(x::QQBarFieldElem)
+    if isinteger(x)
+        m = - first( collect( coefficients( minpoly( x ) ) ) )
+        return string(m)
+    elseif is_rational(x)
+        cf = collect( coefficients( minpoly( x ) ) )
+        return fix_poly_string( string( -cf[1]//cf[2] ) )
+    else
+        return ""
+    end
+end
+
+# represent root of pol of form 1 + ax + (ax)^2 +... +(ax)^n
+function power_sum_tex_rep(x::QQBarFieldElem)
+    mp  = minimal_polynomial(x)
+    if is_power_sum(mp)
+        gen = (collect(coefficients(mp)))[2]
+        n = degree(mp)
+        a = x*gen
+        i = findfirst( j -> QQb(ζ(n+1)^j) == a, 1:n )
+        return string(1//gen, "\\zeta_{", n+1, "}^{", i, "}" ) 
+    else
+        return ""
+    end
+end
+
+function is_power_sum(poly::QQPolyRingElem)
+    (is_geometric_array ∘ collect ∘ coefficients)(poly)
+end
+
+function is_geometric_array(a::Vector{T}) where {T}
+    if length(a) == 0
+        return true
+    elseif length(a) == 1
+        return a[1] == 1
+    else
+        gen = a[2]
+        for i in 2:(length(a)-1)
+            if gen^i != a[i+1]
+                return false
+            end
+            continue
+        end
+        return true
+    end
+end
