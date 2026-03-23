9571339995c63ed487d75938c63548768abb8ea1
Szagha02 <szagha2@gmail.com>
2026-03-23 12:49:58 -0400

    @test_utils -deleted entire file +fixed a little bit (can ignore safely these changes

--- tests/test_utils.jl
+++ tests/test_utils.jl
@@ -19,6 +19,16 @@ function check_equal(actual, expected, msg::AbstractString)
     @test actual == expected
 end
 
+function check_approx(actual, expected, msg::AbstractString; atol=1e-8, rtol=1e-8)
+    ok = isapprox(actual, expected; atol=atol, rtol=rtol)
+    if !ok
+        println("FAILED: ", msg)
+        println("  expected approximately: ", repr(expected))
+        println("  got                   : ", repr(actual))
+    end
+    @test isapprox(actual, expected; atol=atol, rtol=rtol)
+end
+
 function check_throws(f::Function, msg::AbstractString)
     threw = false
     try
